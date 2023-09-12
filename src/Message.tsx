38493d07f3883614f4d2f2f9c79bd4f32d620374
Quan Pham <Quan.Pham@vklink.vn>
2023-09-12 10:33:40 +0700

    getting started

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,1 +1,9 @@
-function Message() {}
+// PascalCasing
+function Message() {
+  //JSX: JavaScript XML
+  const name = "Tí Đù";
+  if (name) return <h1>Hello {name} World</h1>;
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
