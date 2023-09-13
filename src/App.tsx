9a8515068c9b4d90fa51e75da75ef783c343b123
Quan Pham <Quan.Pham@vklink.vn>
2023-09-13 15:07:11 +0700

    updateing status

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,28 @@ import { BsFillCalendarEventFill } from "react-icons/bs";
 import Like from "./Components/Like";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-  const items = ["New York", "Los Angeles", "San Francisco"];
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "Tí Đù",
+    },
+    times: ["Morning"],
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
 
-  const handleCloseAlert = () => {
-    setAlertVisibility(false);
+  const handleClick = () => {
+    //   setGame({ ...game, times: [...game.times, "Evening"] });
+    //   setGame({ ...game, player: { ...game.player, name: "Tèo Am" } });
+    setGame({
+      ...game,
+      items: game.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+    console.log(game);
   };
 
   return (
@@ -22,10 +39,38 @@ function App() {
         onSelectItem={() => {}}
       ></ListGroup>
       <BsFillCalendarEventFill color="red" size="40" /> */}
-      {/* <Button onClick={() => {}}>Supper Button</Button> */}
-      <Like onClick={() => console.log("Clicked")} />
+      <Button onClick={handleClick}>Show</Button>
+      {/* <Like onClick={() => console.log("Clicked")} /> */}
     </div>
   );
 }
+
+// function App() {
+//   const [alertVisible, setAlertVisibility] = useState(false);
+//   const items = ["New York", "Los Angeles", "San Francisco"];
+//   const [isVisible, setVisibility] = useState(false);
+
+//   const handleCloseAlert = () => {
+//     setAlertVisibility(false);
+//   };
+
+//   const handleClick = () => {
+//     setVisibility(true);
+//     console.log(isVisible);
+//   };
+
+//   return (
+//     <div>
+//       {/* <ListGroup
+//         heading="Miami"
+//         items={items}
+//         onSelectItem={() => {}}
+//       ></ListGroup>
+//       <BsFillCalendarEventFill color="red" size="40" /> */}
+//       <Button onClick={handleClick}>Show</Button>
+//       {/* <Like onClick={() => console.log("Clicked")} /> */}
+//     </div>
+//   );
+// }
 
 export default App;
