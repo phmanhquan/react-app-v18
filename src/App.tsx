28f2c4685c5d0728892ee079c98a67d61da6bdbd
Quan Pham <Quan.Pham@vklink.vn>
2023-09-13 16:01:11 +0700

    update object

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ListGroup from "./Components/ListGroup";
 import "./App.css";
 import { BsFillCalendarEventFill } from "react-icons/bs";
 import Like from "./Components/Like";
+import Message from "./Message";
 
 function App() {
   const [game, setGame] = useState({
@@ -17,30 +18,41 @@ function App() {
       { id: 1, title: "Product 1", quantity: 1 },
       { id: 2, title: "Product 2", quantity: 1 },
     ],
+    price: 5,
   });
 
+  const [tags, setTags] = useState(["happy", "cheerful"]);
+
   const handleClick = () => {
+    //// update object
+    // setGame({ ...game, price: 10 });
+    // setGame({ ...game, price: 10, id: 2 });
     //   setGame({ ...game, times: [...game.times, "Evening"] });
-    //   setGame({ ...game, player: { ...game.player, name: "Tèo Am" } });
-    setGame({
-      ...game,
-      items: game.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-    console.log(game);
+    //   setGame({ ...game, player: { ...game.player, name: "Tèo Em" } });
+    // setGame({
+    //   ...game,
+    //   items: game.items.map((item) =>
+    //     item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+    //   ),
+    // });
+    // console.log(game);
+
+    // //add item array
+    // setTags([...tags, "he he"]);
+
+    // // remove item array
+    // setTags(tags.filter((tag) => tag !== "he he"));
+
+    //update item array
+    setTags(tags.map((tag) => (tag === "happy" ? "he he" : tag)));
+
+    console.log(tags);
   };
 
   return (
     <div>
-      {/* <ListGroup
-        heading="Miami"
-        items={items}
-        onSelectItem={() => {}}
-      ></ListGroup>
-      <BsFillCalendarEventFill color="red" size="40" /> */}
       <Button onClick={handleClick}>Show</Button>
-      {/* <Like onClick={() => console.log("Clicked")} /> */}
+      {/* <Message></Message> */}
     </div>
   );
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,9 @@
-// PascalCasing
-function Message() {
-  //JSX: JavaScript XML
-  const name = "Tí Đù";
-  if (name) return <h1>Hello {name} World</h1>;
-  return <h1>Hello World</h1>;
-}
+let count = 0;
+
+const Message = () => {
+  console.log("Call", count);
+  count++;
+  return <div>Message {count}</div>;
+};
 
 export default Message;
