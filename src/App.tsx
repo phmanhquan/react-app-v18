39b4701d796c03aa3d0cb93895fb3c55da75cddb
Quan Pham <Quan.Pham@vklink.vn>
2023-09-18 11:27:23 +0700

    cancleling fetch

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ProductList from "./Components/ProductList";
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, CanceledError } from "axios";
 
 interface User {
   id: number;
@@ -27,29 +27,36 @@ function App() {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/xusers"
-        );
-        setError("");
-        setUsers(res.data);
-      } catch (err) {
-        setUsers([]);
-        setError((err as AxiosError).message);
-      }
-    };
-
-    fetchUsers();
-
-    // // get -> promise -> res / err
-    // axios
-    //   .get<User[]>("https://jsonplaceholder.typicode.com/users")
-    //   .then((res) => {
-    //     setUsers(res.data);
+    // const fetchUsers = async () => {
+    //   try {
+    //     const res = await axios.get<User[]>(
+    //       "https://jsonplaceholder.typicode.com/xusers"
+    //     );
     //     setError("");
-    //   })
-    //   .catch((err) => setError(err.message));
+    //     setUsers(res.data);
+    //   } catch (err) {
+    //     setUsers([]);
+    //     setError((err as AxiosError).message);
+    //   }
+    // };
+
+    // fetchUsers();
+
+    const controller = new AbortController();
+
+    // get -> promise -> res / err
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setError("");
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
   }, []);
 
   return (
