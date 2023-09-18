dbd9bc5c661a3f1401742babef82796201009937
Quan Pham <Quan.Pham@vklink.vn>
2023-09-18 11:53:47 +0700

    delete data

--- src/App.tsx
+++ src/App.tsx
@@ -66,13 +66,35 @@ function App() {
     // return controller.abort();
   }, []);
 
+  const deleteUser = (user: User) => {
+    setUsers(users.filter((u) => u.id !== user.id));
+    const originalUsers = [...users];
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <div>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}> {user.name} </li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </div>
