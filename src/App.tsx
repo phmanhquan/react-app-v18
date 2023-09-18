f588f322400ac625dff25c12dc7c1546326a0c23
Quan Pham <Quan.Pham@vklink.vn>
2023-09-18 10:59:05 +0700

    work with async and await

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ProductList from "./Components/ProductList";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 interface User {
   id: number;
@@ -27,13 +27,29 @@ function App() {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        setUsers(res.data);
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/xusers"
+        );
         setError("");
-      })
-      .catch((err) => setError(err.message));
+        setUsers(res.data);
+      } catch (err) {
+        setUsers([]);
+        setError((err as AxiosError).message);
+      }
+    };
+
+    fetchUsers();
+
+    // // get -> promise -> res / err
+    // axios
+    //   .get<User[]>("https://jsonplaceholder.typicode.com/users")
+    //   .then((res) => {
+    //     setUsers(res.data);
+    //     setError("");
+    //   })
+    //   .catch((err) => setError(err.message));
   }, []);
 
   return (
