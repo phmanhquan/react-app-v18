62ea799b79db649bab7c57a34d94b02062c03564
Quan Pham <Quan.Pham@vklink.vn>
2023-09-25 10:30:29 +0700

    add and use react query

--- src/App.tsx
+++ src/App.tsx
@@ -19,81 +19,86 @@ import ProductList from "./Components/ProductList";
 import apiClient, { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
 import useUsers from "./hooks/useUsers";
+import TodoList from "./react-query/TodoList";
 
 function App() {
-  const { users, error, isLoading, setUsers, setError } = useUsers();
-
-  const deleteUser = (user: User) => {
-    setUsers(users.filter((u) => u.id !== user.id));
-    const originalUsers = [...users];
-
-    userService.delete(user.id).catch((err) => {
-      setError(err.message);
-      setUsers(originalUsers);
-    });
-  };
-
-  const addUser = () => {
-    const originalUsers = [...users];
-    const newUser = { id: 0, name: "jacky" };
-    setUsers([newUser, ...users]);
-
-    userService
-      .create(newUser)
-      .then(({ data: savedUser }) => {
-        setUsers([savedUser, ...users]);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setUsers(originalUsers);
-      });
-  };
-
-  const updateUser = (user: User) => {
-    const updatedUser = { ...user, name: user.name + "!" };
-    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
-    const originalUsers = [...users];
-
-    userService.update(updatedUser).catch((err) => {
-      setError(err.message);
-      setUsers(originalUsers);
-    });
-  };
-
-  return (
-    <div>
-      {error && <p className="text-danger">{error}</p>}
-      {isLoading && <div className="spinner-border"></div>}
-      <button className="btn btn-primary mb-3" onClick={() => addUser()}>
-        Add
-      </button>
-      <ul className="list-group">
-        {users.map((user) => (
-          <li
-            key={user.id}
-            className="list-group-item d-flex justify-content-between"
-          >
-            {user.name}
-            <div>
-              <button
-                className="btn btn-outline-secondary mx-1"
-                onClick={() => updateUser(user)}
-              >
-                Update
-              </button>
-              <button
-                className="btn btn-outline-danger"
-                onClick={() => deleteUser(user)}
-              >
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <TodoList></TodoList>;
 }
+
+// function App() {
+//   const { users, error, isLoading, setUsers, setError } = useUsers();
+
+//   const deleteUser = (user: User) => {
+//     setUsers(users.filter((u) => u.id !== user.id));
+//     const originalUsers = [...users];
+
+//     userService.delete(user.id).catch((err) => {
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     });
+//   };
+
+//   const addUser = () => {
+//     const originalUsers = [...users];
+//     const newUser = { id: 0, name: "jacky" };
+//     setUsers([newUser, ...users]);
+
+//     userService
+//       .create(newUser)
+//       .then(({ data: savedUser }) => {
+//         setUsers([savedUser, ...users]);
+//       })
+//       .catch((err) => {
+//         setError(err.message);
+//         setUsers(originalUsers);
+//       });
+//   };
+
+//   const updateUser = (user: User) => {
+//     const updatedUser = { ...user, name: user.name + "!" };
+//     setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+//     const originalUsers = [...users];
+
+//     userService.update(updatedUser).catch((err) => {
+//       setError(err.message);
+//       setUsers(originalUsers);
+//     });
+//   };
+
+//   return (
+//     <div>
+//       {error && <p className="text-danger">{error}</p>}
+//       {isLoading && <div className="spinner-border"></div>}
+//       <button className="btn btn-primary mb-3" onClick={() => addUser()}>
+//         Add
+//       </button>
+//       <ul className="list-group">
+//         {users.map((user) => (
+//           <li
+//             key={user.id}
+//             className="list-group-item d-flex justify-content-between"
+//           >
+//             {user.name}
+//             <div>
+//               <button
+//                 className="btn btn-outline-secondary mx-1"
+//                 onClick={() => updateUser(user)}
+//               >
+//                 Update
+//               </button>
+//               <button
+//                 className="btn btn-outline-danger"
+//                 onClick={() => deleteUser(user)}
+//               >
+//                 Delete
+//               </button>
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
 // const connect = () => console.log("connecting");
 // const disconnect = () => console.log("disconnecting");
---
+++ src/react-query/TodoList.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const TodoList = () => {
+  const fetchTodoList = () =>
+    axios
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.data);
+
+  const { data: todoList, error } = useQuery<Todo[], Error>({
+    queryKey: ["todoList"],
+    queryFn: fetchTodoList,
+  });
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <ul className="list-group">
+      {todoList?.map((todo) => (
+        <li key={todo.id} className="list-group-item">
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
