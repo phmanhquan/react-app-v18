d374e47a189f9ce32b0a176cc248faac7b560056
Quan Pham <Quan.Pham@vklink.vn>
2023-09-27 17:08:33 +0700

    working with contex

--- src/App.tsx
+++ src/App.tsx
@@ -29,15 +29,20 @@ import tasksReducer from "./state-management/reducers/tasksReducer";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import TasksContext from "./state-management/contexts/tasksContext";
+import authReducer from "./state-management/reducers/authReducer";
+import AuthContext from "./state-management/contexts/authContext";
 
 function App() {
-  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
+  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <TasksContext.Provider value={{ tasks, dispatch }}>
-      <NavBar></NavBar>
-      <HomePage></HomePage>
-    </TasksContext.Provider>
+    <AuthContext.Provider value={{ auth: user, dispatch: authDispatch }}>
+      <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+        <NavBar></NavBar>
+        <HomePage></HomePage>
+      </TasksContext.Provider>
+    </AuthContext.Provider>
   );
 }
 
