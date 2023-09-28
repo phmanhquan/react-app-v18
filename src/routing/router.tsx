9db19e87f6b8423c61b0d7091d092a53c49424e7
Quan Pham <Quan.Pham@vklink.vn>
2023-09-28 17:05:39 +0700

    nested routes

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -3,11 +3,18 @@ import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import ContactPage from "./ContactPage";
 import UserDetailPage from "./UserDetailPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage></HomePage> },
-  { path: "/users", element: <UserListPage></UserListPage> },
-  { path: "/users/:id", element: <UserDetailPage></UserDetailPage> },
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      { index: true, element: <HomePage></HomePage> },
+      { path: "users", element: <UserListPage></UserListPage> },
+      { path: "users/:id", element: <UserDetailPage></UserDetailPage> },
+    ],
+  },
   { path: "/contact", element: <ContactPage></ContactPage> },
 ]);
 
