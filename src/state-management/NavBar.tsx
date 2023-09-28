46ebf4e39c5a2794745aef4259e6c3edba26e065
Quan Pham <Quan.Pham@vklink.vn>
2023-09-28 13:55:16 +0700

    add zustand devtool

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -2,10 +2,10 @@ import { LoginStatus } from "./auth";
 import useTasksStore from "./tasks/store";
 
 const NavBar = () => {
-  const { tasks } = useTasksStore();
+  const taskTotal = useTasksStore((s) => s.tasks.length);
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">{taskTotal}</span>
       <LoginStatus />
     </nav>
   );
